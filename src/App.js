2904f50dee1659839e2f539155a64bd07be5df85
Mannie <manniecobham@yahoo.com>
2021-10-11 12:19:21 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import {HashRouter as Router, Switch, Route } from 'react-router-dom'; 
+import Add from './pages/Add'
+import Learn from "./pages/Learn";
+const App = () => {
+    return(
+    <>
+    <Router>
+      
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/add"  component={Add} />
+        <Route path="/Learn" component={Learn} />
+      </Switch>
+    </Router>
+    </>
+  );
+};
+
+export default App;
+
